5d3c2a361b3e474f10d32c385ee8530a909952aa
Mojtaba Rabiei <m.rabiei.project@gmail.com>
2015-02-11 17:50:36 +0330

    Add JNotify lib & watchCreate method for tracking created file & directory

--- src/com/newvertex/mp3tagchanger/Mp3TagChanger.java
+++ src/com/newvertex/mp3tagchanger/Mp3TagChanger.java
@@ -1,9 +1,29 @@
 package com.newvertex.mp3tagchanger;
 
+import net.contentobjects.jnotify.JNotify;
+import net.contentobjects.jnotify.JNotifyAdapter;
+import net.contentobjects.jnotify.JNotifyException;
+
 public class Mp3TagChanger {
 
     public static void main(String[] args) {
-        
+
     }
-    
+
+    private void watchCreate(String path) throws JNotifyException {
+        int mask = JNotify.FILE_CREATED;
+        boolean watchSubTree = true;
+
+        int watchId = JNotify.addWatch(path, mask, watchSubTree, new Listener());
+    }
+
+    private class Listener extends JNotifyAdapter {
+
+        @Override
+        public void fileCreated(int wd, String rootPath, String name) {
+            System.out.println("Created: " + rootPath + name);
+        }
+
+    }
+
 }
