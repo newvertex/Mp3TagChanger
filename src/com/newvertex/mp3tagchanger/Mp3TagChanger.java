7689a1b5bf843bb657dc2600e5c9a5c354d9f7a2
Mojtaba Rabiei <m.rabiei.project@gmail.com>
2015-02-11 18:02:55 +0330

    Some clean up

--- src/com/newvertex/mp3tagchanger/Mp3TagChanger.java
+++ src/com/newvertex/mp3tagchanger/Mp3TagChanger.java
@@ -12,22 +12,30 @@ public class Mp3TagChanger {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter directory path:");
-        String path = new Scanner(System.in).nextLine();
+        new Mp3TagChanger().watchCreate(getPath());
 
-        if (!path.endsWith(File.separator)) {
-            path += File.separator;
-        }
+        mainLoop();
+    }
 
-        new Mp3TagChanger().watchCreate(path);
-
+    private static void mainLoop() {
+        System.out.println("For close the program type: exit");
         while (true) {
             Scanner scanner = new Scanner(System.in);
             if (scanner.nextLine().toLowerCase().equals("exit")) {
                 break;
+            } else {
+                System.out.println("For close the program type: exit");
             }
         }
+    }
 
+    private static String getPath() {
+        System.out.println("Enter directory path:");
+        String path = new Scanner(System.in).nextLine();
+        if (!path.endsWith(File.separator)) {
+            path += File.separator;
+        }
+        return path;
     }
 
     private void watchCreate(String path) {
