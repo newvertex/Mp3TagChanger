63af450f5354f14420a60cca16be47962e87dfab
Mojtaba Rabiei <m.rabiei.project@gmail.com>
2015-02-11 21:53:37 +0330

    Add mainMenu to main class

--- src/com/newvertex/mp3tagchanger/Mp3TagChanger.java
+++ src/com/newvertex/mp3tagchanger/Mp3TagChanger.java
@@ -1,18 +1,44 @@
 package com.newvertex.mp3tagchanger;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mp3TagChanger {
 
     public static void main(String[] args) {
-
-        Watcher.watchCreate(getPath());
-
-        mainLoop();
+        new Mp3TagChanger().mainMenu();
     }
 
-    private static void mainLoop() {
+    private void mainMenu() {
+        while (true) {
+            System.out.println("1. Watching for new file and change tag.\n"
+                    + "2. Change all files tag.\n"
+                    + "3. Set tsgs.\n"
+                    + "4. Exit.");
+            System.out.print("$ ");
+            String item = new Scanner(System.in).nextLine();
+            switch (item) {
+                case "1":
+                    watching();
+                    break;
+                case "2":
+                    ArrayList<Path> files = GetAllFiles.getFiles(getPath());
+                    System.out.println("Files: " + files);
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    System.exit(0);
+                    break;
+                default:
+            }
+        }
+    }
+
+    private static void watching() {
+        Watcher.watchCreate(getPath());
         System.out.println("For close the program type: exit");
         while (true) {
             Scanner scanner = new Scanner(System.in);
