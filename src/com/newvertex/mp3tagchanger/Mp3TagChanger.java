4accddb2047dc253f519cf0cbcaab2c0d706cca6
Mojtaba Rabiei <m.rabiei.project@gmail.com>
2015-02-11 17:56:55 +0330

    Use try-catch instead of throws exception

--- src/com/newvertex/mp3tagchanger/Mp3TagChanger.java
+++ src/com/newvertex/mp3tagchanger/Mp3TagChanger.java
@@ -2,6 +2,8 @@ package com.newvertex.mp3tagchanger;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.contentobjects.jnotify.JNotify;
 import net.contentobjects.jnotify.JNotifyAdapter;
 import net.contentobjects.jnotify.JNotifyException;
@@ -12,18 +14,24 @@ public class Mp3TagChanger {
 
         System.out.println("Enter directory path:");
         String path = new Scanner(System.in).nextLine();
-        
+
         if (!path.endsWith(File.separator)) {
             path += File.separator;
         }
 
+        new Mp3TagChanger().watchCreate(path);
+
     }
 
-    private void watchCreate(String path) throws JNotifyException {
+    private void watchCreate(String path) {
         int mask = JNotify.FILE_CREATED;
         boolean watchSubTree = true;
 
-        int watchId = JNotify.addWatch(path, mask, watchSubTree, new Listener());
+        try {
+            int watchId = JNotify.addWatch(path, mask, watchSubTree, new Listener());
+        } catch (JNotifyException ex) {
+            Logger.getLogger(Mp3TagChanger.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     private class Listener extends JNotifyAdapter {
